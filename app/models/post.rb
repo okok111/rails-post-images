246b7edf317a4f8ac99b0eede3ecca0616d91b6e
okok111 <1for.rails@gmail.com>
2022-10-26 03:46:23 +0900

    uploadersに編集

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
     mount_uploader :image, ImageUploader
+    mount_uploaders :photographs, ImageUploader
     has_many :likes, dependent: :destroy
     has_many :liked_users, through: :likes, source: :user
     has_many :comments, dependent: :destroy
