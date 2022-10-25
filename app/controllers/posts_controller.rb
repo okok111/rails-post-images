41f2a4b766b0c75c8b4497be59a6cd0f9abe9b4e
okok111 <1for.rails@gmail.com>
2022-10-26 03:30:43 +0900

    ストロングパラメータに配列で許可させる

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -46,7 +46,7 @@ class PostsController < ApplicationController
 
   private
   def post_params
-      params.require(:post).permit(:title, :contents ,:image)
+      params.require(:post).permit(:title, :contents ,:image ,photographs:[])
   end
 
 end
